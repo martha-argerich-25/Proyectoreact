ee2255308f9e2fdbdc23a61b78faf1c244bea686
rodrigo25 <rodrigoperettisoles@gmail.com>
2022-10-12 19:59:05 -0300

    se agrego evento

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,20 +1,25 @@
 
-import Counter from "../counter/counter"
+import Counter from "../counter/counter";
+import{useState} from 'react';
+import { Link } from "react-router-dom";
 
 
 // CARD// componente de visualizacion
 const ItemDetail = ({name,price,stock,description,img,category})=>{
    
+const [goCart,setGoCart] = useState (false);
+
+
     const handLeOnadd =()=>{
-        console.log("se agrego al carrito")
+       setGoCart(true);
+
+
       }
 
   
 return(
   
     <div className='body'>
-           
-   
     <h2 className='title'>{name}</h2>
 
      <h2>Precio {price}$</h2>
@@ -22,10 +27,11 @@ return(
      <p>{description}</p>
     <p>categoria {category}</p>
     <img src={img} alt={'name'} style={{width:400}}/>
+  {
+      //si es verdadero que nos lleve al carrito y si es falso que renderice
+      goCart ?<Link to='/cart'>finalizar compra</Link>:<Counter onAdd={handLeOnadd}/>
 
-
-    <Counter onAdd={handLeOnadd}/>
-  
+  }
 
 </div>)
 
